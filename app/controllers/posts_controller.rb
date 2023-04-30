9598cf2672eaf66ade5ef8e39b7516dd26da5475
Samuel O. Asare <asare11samuel@gmail.com>
2023-04-30 23:58:02 +0000

    create post. strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,20 @@ class PostsController < ApplicationController
     post_id = params[:id]
     @post = Post.find(post_id)
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title,:body)
+    end
   
 end
