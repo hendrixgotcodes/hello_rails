395174d76aaabf57b8fe0cf6d77774de3d39baea
Samuel O. Asare <asare11samuel@gmail.com>
2023-05-01 00:27:38 +0000

    feat: update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to @posts
+      redirect_to @post
     else
       render :edit, status: :unprocessable_entity
     end
